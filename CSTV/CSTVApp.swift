4ef9b2f5f0d93a730bd84259ccc4bdbe2a69aac7
Lucas Brandão <lucasbranddao@hotmail.com>
2024-08-24 14:05:13 -0300

    Initial Commit

---
+++ CSTV/CSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVApp.swift
+//  CSTV
+//
+//  Created by Lucas Brandão on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
